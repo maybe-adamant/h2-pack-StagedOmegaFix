06431cb3d1aad95c95a25173bee28b9618da8576
maybe-adamant <maybe.adamant@gmail.com>
2026-03-25 16:23:18 -0400

    changing discovery flag from bool to string. preparing to moving core to a framework

--- src/main.lua
+++ src/main.lua
@@ -27,7 +27,7 @@ public.definition = {
     tooltip  = "Fixes Axe OAtk, Blade OSpec not benefiting correctly from channeling bonus.",
     default  = true,
     dataMutation = true,
-    modpackModule = true,
+    modpack = "h2-modpack",
 }
 
 -- =============================================================================
