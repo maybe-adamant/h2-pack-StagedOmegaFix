67018d771300297e7f07543fdcf2d37c9a75617f
maybe-adamant <maybe.adamant@gmail.com>
2026-03-26 16:16:16 -0400

    removing hard dependency on coordinator mod name

--- src/main.lua
+++ src/main.lua
@@ -57,8 +57,8 @@ modutil.once_loaded.game(function()
     loader.load(function()
         import_as_fallback(rom.game)
         registerHooks()
-        if lib.isEnabled(config) then apply() end
-        if public.definition.dataMutation and not mods['adamant-Modpack_Core'] then
+        if lib.isEnabled(config, public.definition.modpack) then apply() end
+        if public.definition.dataMutation and not lib.isCoordinated(public.definition.modpack) then
             SetupRunData()
         end
     end)
