3edd5b1155c90364aa1052a5403312d2008f4b49
maybe-adamant <maybe.adamant@gmail.com>
2026-03-28 16:05:25 -0400

    updating submodules

--- src/main.lua
+++ src/main.lua
@@ -27,7 +27,7 @@ public.definition = {
     tooltip  = "Fixes Axe OAtk, Blade OSpec not benefiting correctly from channeling bonus.",
     default  = true,
     dataMutation = true,
-    modpack = "h2-modpack",
+    modpack = "speedrun",
 }
 
 -- =============================================================================
